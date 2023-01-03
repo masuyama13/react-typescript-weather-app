5629a9949aca04ec3e6018d09ce5ed684c6e7480
masuyama13 <masterxwebm13@gmail.com>
2023-01-03 15:39:17 +0900

    APIリクエストできるようにする

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,17 @@
+import { useState } from 'react';
+
 const Form = () => {
+    const [city, setCity] = useState<string>("");
+    const getWeather = (e: any) => {
+        e.preventDefault();
+        fetch("https://api.weatherapi.com/v1/current.json?key=64fc006feb974ab39b261959230301&q=London&aqi=no")
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }
     return (
         <form>
-            <input type="text" name="city" placeholder="都市名"/>
-            <button type="submit">Get Weather</button>
+            <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
+            <button type="submit" onClick={getWeather}>Get Weather</button>
         </form>
     );
 };
